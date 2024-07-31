6c812f7e055f4a3465119946d33216b663e93bfe
AdolfoVLA <avargassandoval@vlacademy.com>
2024-07-30 20:07:35 -0600

    Ajuste

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  });
+  }); 
 
 app.use(express.json());
 
